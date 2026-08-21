f730b03fc373cfb024142c15ff01e4f1603e4a7a
Yash <kumawatyash060@gmail.com>
2026-08-21 17:01:58 +0530

    Adding loading spinner & variable pageSize

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
   constructor() {
@@ -12,59 +13,66 @@ export class News extends Component {
       totalResults: 0,
     };
   }
-
   async componentDidMount() {
-    let url = `https://newsapi.org/v2/everything?q=apple&from=2026-08-20&to=2026-08-20&sortBy=popularity&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&page=1&pageSize=12`;
-
+    let url = `https://newsapi.org/v2/everything?q=apple&from=2026-08-20&to=2026-08-20&sortBy=popularity&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
-    console.log(parsedData);
     this.setState({
       articles: parsedData.articles || [],
       totalResults: parsedData.totalResults,
+      loading: false
     });
   }
 
+  // -------                                              NEXT CLICK FUNCTION
   handleNextClick = async () => {
     let nextPage = this.state.page + 1;
 
-    if (nextPage > Math.ceil(this.state.totalResults / 12)) {
-      return;
+    if (
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
+    ) {
+      let url = `https://newsapi.org/v2/everything?q=apple&from=2026-08-20&to=2026-08-20&sortBy=popularity&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&page=${nextPage}&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        articles: parsedData.articles || [],
+        page: nextPage,
+        loading: false
+      });
     }
-
-    let url = `https://newsapi.org/v2/everything?q=apple&from=2026-08-20&to=2026-08-20&sortBy=popularity&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&page=${nextPage}&pageSize=12`;
-
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState({
-      articles: parsedData.articles || [],
-      page: nextPage,
-    });
   };
+
+  // -------                                              PREVIOUS CLICK FUNCTION
   handlePrevClick = async () => {
     let prevPage = this.state.page - 1;
     if (this.state.page <= 1) {
     } else {
-      let url = `https://newsapi.org/v2/everything?q=apple&from=2026-08-20&to=2026-08-20&sortBy=popularity&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&page=${this.state.page - 1}&pageSize=12`;
-
+      let url = `https://newsapi.org/v2/everything?q=apple&from=2026-08-20&to=2026-08-20&sortBy=popularity&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
       let data = await fetch(url);
       let parsedData = await data.json();
       console.log(parsedData);
       this.setState({
         articles: parsedData.articles || [],
         page: prevPage,
-      });
+        loading: false
+      }); 
     }
   };
 
   render() {
     return (
       <div className="container my-3">
-        <h2>News - Headlines</h2>
-
+        <h2 className="text-center">News - Headlines</h2>
+        {this.state.loading && <Spinner/>}
         <div className="row">
-          {this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             return (
               <div className="col-md-3" key={element.url}>
                 <NewsItem
@@ -91,6 +99,9 @@ export class News extends Component {
           </button>
 
           <button
+            disabled={
+              this.state.page + 1 > Math.ceil(this.state.totalResults / 12)
+            }
             className="btn btn-secondary"
             onClick={this.handleNextClick}
             type="button"

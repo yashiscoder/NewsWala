5ec8188c90aeade47601d128f2aeb759c97e42dc
Yash <kumawatyash060@gmail.com>
2026-08-21 15:18:26 +0530

    Adding Previous & Next Buttons to populate NewsItems

--- src/components/News.js
+++ src/components/News.js
@@ -2,88 +2,59 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  articles = [
-    {
-      "source": {
-        "id": "null",
-        "name": "SamMobile",
-      },
-      "author": "Asif Iqbal Shaik",
-      "title":
-        "Samsung will now charge more to make 5nm or better chips for its clients",
-      "description":
-        "Samsung makes advanced semiconductor chips not only for itself but also for other companies. Its contract chipmaking business faced a lot of issues over the past four to five years, but it has picked up so much momentum in the past year that the company, whic…",
-      "url": "https://www.sammobile.com/news/samsung-will-now-charge-more-to-make-5nm-or-better-chips-for-its-clients/",
-      "urlToImage":
-        "https://www.sammobile.com/wp-content/uploads/2026/08/Samsung-Foundry-Chip-Fabrication.jpg",
-      "publishedAt": "2026-08-20T05:16:55Z",
-      "content":
-        "Samsung makes advanced semiconductor chips not only for itself but also for other companies. Its contract chipmaking business faced a lot of issues over the past four to five years, but it has picked… [+1739 chars]",
-    },
-    {
-      "source": {
-        "id": "null",
-        "name": "Autocar",
-      },
-      "author": "Nick Gibbs",
-      "title":
-        "Chinese car makers take a fifth of the UK market – can they keep growing?",
-      "description":
-        "Affordable electrified vehicles power a huge surge in Chinese car sales, as legacy firms struggle to stay competitive\n\nChinese brands accounted for almost a fifth of all UK registrations in July as newcomers Omoda-Jaecoo and BYD both celebrated their 100,000t…",
-      "url": "https://www.autocar.co.uk/car-news/business-car-sales/chinese-car-makers-take-fifth-uk-market-%E2%80%93-can-they-keep-growing",
-      "urlToImage":
-        "https://images.cdn.autocar.co.uk/sites/autocar.co.uk/files/images/car-reviews/first-drives/legacy/4._jaecoo_range_on_road.jpg",
-      "publishedAt": "2026-08-20T04:00:00Z",
-      "content":
-        "Chinese brands accounted for almost a fifth of all UK registrations in July as newcomers Omoda-Jaecoo and BYD both celebrated their 100,000th UK sale in quick succession.\r\nA mix of keen prices, elect… [+181 chars]",
-    },
-    {
-      "source": {
-        "id": "techradar",
-        "name": "TechRadar",
-      },
-      "author": "Leon Poultney",
-      "title":
-        "Tesla’s Robotaxi gets caught crashing through bollards, just as it readies the purpose-built Cybercab for public roads",
-      "description":
-        "Tesla Robotaxi gets confused by bollards — just as the Cybercab gets ready for the road",
-      "url": "https://www.techradar.com/vehicle-tech/hybrid-electric-vehicles/teslas-robotaxi-gets-caught-crashing-through-bollards-just-as-it-readies-the-purpose-built-cybercab-for-public-roads",
-      "urlToImage":
-        "https://cdn.mos.cms.futurecdn.net/JYHq2wQHiozy684Uz3oCa-1700-80.jpg",
-      "publishedAt": "2026-08-20T03:30:00Z",
-      "content":
-        '<ul><li>Texas Robotaxi passenger films car hitting plastic bollards</li><li>Company claims "impeccable" Robotaxi safety record </li><li>Tesla is preparing Cybercabs for public roads imminently</li></… [+3590 chars]',
-    },
-    {
-      "source": {
-        "id": "null",
-        "name": "Pypi.org",
-      },
-      "author": null,
-      "title": "wallmonitor added to PyPI",
-      "description":
-        "Local-only monitoring and recording UI for a Tesla Wall Connector Gen 3",
-      "url": "https://pypi.org/project/wallmonitor/",
-      "urlToImage": null,
-      "publishedAt": "2026-08-20T01:01:34Z",
-      "content":
-        "A local-only companion for the Tesla Wall Connector Gen 3 that grew well\r\nbeyond reading data off the charger: it records everything the device\r\nreports, turns that history into a thermal model fitte… [+4082 chars]",
-    },
-  ];
   constructor() {
     super();
+
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false,
+      page: 1,
     };
   }
 
-  async componentDidMount(){
-  let url = "https://newsapi.org/v2/everything?q=apple&from=2026-08-20&to=2026-08-20&sortBy=popularity&apiKey=eba2cc36777248e1839d0373ac856e1d";
-  let data = await fetch (url);
-  let parsedData = await data.json();
-  this.setState({articles: parsedData.articles});
-}
+  async componentDidMount() {
+    let url =`https://newsapi.org/v2/everything?q=apple&from=2026-08-20&to=2026-08-20&sortBy=popularity&apiKey=eba2cc36777248e1839d0373ac856e1d&page=1&pageSize=12`;
+
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      articles: parsedData.articles, totalResults: parsedData.totalResults
+    });
+  }
+
+  handleNextClick = async () => {
+    let nextPage = this.state.page + 1;
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+    }else{
+    let url = `https://newsapi.org/v2/everything?q=apple&from=2026-08-20&to=2026-08-20&sortBy=popularity&apiKey=eba2cc36777248e1839d0373ac856e1d&page=${this.state.page+1}&pageSize=12`;
+
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      articles: parsedData.articles,
+      page: nextPage,
+    });
+  }
+  };
+
+  handlePrevClick = async () => {
+    let prevPage = this.state.page - 1;
+    if(this.state.page - 1 <Math.ceil(this.state.totalResults)){
+
+    }else{
+    let url = `https://newsapi.org/v2/everything?q=apple&from=2026-08-20&to=2026-08-20&sortBy=popularity&apiKey=eba2cc36777248e1839d0373ac856e1d&page=${this.state.page - 1}&pageSize=12`;
+
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      articles: parsedData.articles,
+      page: prevPage,
+    });}
+  };
 
   render() {
     return (
@@ -92,20 +63,42 @@ export class News extends Component {
 
         <div className="row">
           {this.state.articles.map((element) => {
-            return(
-            <div className="col-md-3" key={element.url}>
-              <NewsItem
-                key = {element.url}
-                title={element.title?element.title.slice(0,45):""}
-                description={element.description?element.description.slice(0,80):""}
-                imageUrl={element.urlToImage?element.urlToImage:""}
-                newsUrl = {element.newsUrl?element.url:""}
-              />
-            </div>);
+            return (
+              <div className="col-md-3" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 45) : ""}
+                  description={
+                    element.description ? element.description.slice(0, 80) : ""
+                  }
+                  imageUrl={element.urlToImage ? element.urlToImage : ""}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
           })}
+        </div>
+
+        <div className="d-flex justify-content-between my-3">
+          <button
+            disabled={this.state.page <= 1}
+            className="btn btn-secondary"
+            onClick={this.handlePrevClick}
+            type="button"
+          >
+            &larr; Previous
+          </button>
+
+          <button
+            className="btn btn-secondary"
+            onClick={this.handleNextClick}
+            type="button"
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
   }
 }
+
 export default News;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
 
 export class Newsitem extends Component {
-array = [];
-constructor(){
-  super();
-}
+  array = [];
+  // constructor() {
+  //   super();
+  // }
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div className="my-2">
-        <div className="card" style={{width: "18rem"}}>
-          <img src={!imageUrl?"https://mediaproxy.tvtropes.org/width/1200/https://static.tvtropes.org/pmwiki/pub/images/nevergonnagiveyouup.jpg":imageUrl} className="card-img-top" alt="IMG" />
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={
+              !imageUrl
+                ? "https://mediaproxy.tvtropes.org/width/1200/https://static.tvtropes.org/pmwiki/pub/images/nevergonnagiveyouup.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="IMG"
+          />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
-            <p className="card-text">
-              {description}
-            </p>
-            <a href={newsUrl} target="_blank" className="btn btn sm btn-primary">
+            <p className="card-text">{description}</p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-outline-success"
+            >
               Go to news
             </a>
           </div>
